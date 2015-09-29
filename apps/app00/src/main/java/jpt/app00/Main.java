513d55e77400a3a3079445b9fbf80c64875adcda
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-09-29 11:35:40 +0200

    feat(apps/app00): port number and queue size may be changed in command line

--- apps/app00/src/main/java/jpt/app00/Main.java
+++ apps/app00/src/main/java/jpt/app00/Main.java
@@ -31,9 +31,25 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Main {
 
+  private static final int DEFAULT_QUEUE_SIZE = 0;
+  private static final int DEFAULT_PORT_NUMBER = 6666;
+
   public static void main(String[] args) throws IOException {
-    int portNumber = 6666;
-    int queueSize = 0;
+    int portNumber = DEFAULT_PORT_NUMBER;
+    int queueSize = DEFAULT_QUEUE_SIZE;
+    for (String arg : args) {
+      if (arg.startsWith("-p")) {
+        portNumber = Integer.parseInt(arg.replaceFirst("\\-p", ""));
+      } else if (arg.startsWith("-q")) {
+        queueSize = Integer.parseInt(arg.replaceFirst("\\-q", ""));
+      } else {
+        System.err.println("Usage: java " + Main.class.getName() + " [option...]");
+        System.err.println("Options: ");
+        System.err.println("    -p<i>  change the listen port number (" + DEFAULT_PORT_NUMBER +")");
+        System.err.println("    -q<i>  change the queue size (" + DEFAULT_QUEUE_SIZE + ")");
+        System.exit(1);
+      }
+    }
     HttpServer server = HttpServer.create(new InetSocketAddress(portNumber), queueSize);
 
     server.createContext("/", new Handler());
