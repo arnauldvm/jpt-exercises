f6ad14a15cd9fa587faf5d6c4978ae4f9a772277
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-10-14 13:30:39 +0200

    fix(apps/app01): add missing compilation for data package

--- apps/app01/compile.bat
+++ apps/app01/compile.bat
@@ -1,4 +1,4 @@
 @CALL ..\setenv.bat
 
 @MKDIR target\classes
-@javac -classpath target/classes -d target/classes src/main/java/jpt/app01/*.java
+@javac -classpath target/classes -d target/classes src/main/java/jpt/app01/data/*.java src/main/java/jpt/app01/*.java
