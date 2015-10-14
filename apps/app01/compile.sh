6ad4587052773c213874a4661843f71f82d165a1
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-10-14 14:00:17 +0200

    fix(apps/app01): fix source location for resources copying

--- apps/app01/compile.sh
+++ apps/app01/compile.sh
@@ -5,4 +5,4 @@ popd
 
 mkdir -p target/classes
 javac -classpath target/classes -d target/classes src/main/java/jpt/app01/data/*.java src/main/java/jpt/app01/*.java
-cp -rv src/main/resources target/classes
+cp -rv src/main/resources/. target/classes
