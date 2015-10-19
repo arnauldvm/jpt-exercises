003d9ead3fdc475f0a18344788faef124b907bb2
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-10-20 01:06:42 +0200

    fix(apps/app01): Limit size of heap (min and max)

--- apps/app01/runserver.bat
+++ apps/app01/runserver.bat
@@ -4,5 +4,6 @@
 @MKDIR %logs_dir%
 @FOR /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined ts set ts=%%x
 @SET "gclog_options=-Xloggc:%logs_dir%\gc-%ts%.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
+@SET "heap_options=-Xms1g -Xmx2g"
 
-java -classpath target/classes %gclog_options% jpt.app01.Main -p7666 -q0
+java -classpath target/classes %gclog_options% %heap_options% jpt.app01.Main -p7666 -q0
--- apps/app01/runserver.sh
+++ apps/app01/runserver.sh
@@ -7,5 +7,6 @@ logs_dir="./logs"
 mkdir -p "$logs_dir"
 ts=$(date +%Y%m%d-%H%M%S)
 gclog_options="-Xloggc:$logs_dir/gc-$ts.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
+heap_options="-Xms1g -Xmx2g"
 
-java -classpath target/classes $gclog_options jpt.app01.Main -p7666 -q0
+java -classpath target/classes $gclog_options $heap_options jpt.app01.Main -p7666 -q0
