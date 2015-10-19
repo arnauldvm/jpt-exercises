7e5ee45de7e5dc957318f512b00930a7d3d18267
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-10-20 01:05:39 +0200

    add(apps/app01): Generate GC log

--- apps/app01/runserver.bat
+++ apps/app01/runserver.bat
@@ -1,3 +1,8 @@
-CALL ..\setenv.bat
+@CALL ..\setenv.bat
 
-java -classpath target/classes jpt.app01.Main -p7666 -q0
+@SET logs_dir=.\logs
+@MKDIR %logs_dir%
+@FOR /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined ts set ts=%%x
+@SET "gclog_options=-Xloggc:%logs_dir%\gc-%ts%.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
+
+java -classpath target/classes %gclog_options% jpt.app01.Main -p7666 -q0
--- apps/app01/runserver.sh
+++ apps/app01/runserver.sh
@@ -3,4 +3,9 @@ pushd ..
 . setenv.sh
 popd
 
-java -classpath target/classes jpt.app01.Main -p7666 -q0
+logs_dir="./logs"
+mkdir -p "$logs_dir"
+ts=$(date +%Y%m%d-%H%M%S)
+gclog_options="-Xloggc:$logs_dir/gc-$ts.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
+
+java -classpath target/classes $gclog_options jpt.app01.Main -p7666 -q0
