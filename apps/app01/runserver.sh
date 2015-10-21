26b25aaaadecb9f23761d3ce2adfcabf03329660
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-10-21 04:01:14 +0200

    feat(apps/app01): add logging configuration

--- apps/app01/runserver.sh
+++ apps/app01/runserver.sh
@@ -8,5 +8,6 @@ mkdir -p "$logs_dir"
 ts=$(date +%Y%m%d-%H%M%S)
 gclog_options="-Xloggc:$logs_dir/gc-$ts.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
 heap_options="-Xms1g -Xmx2g"
+logging_options="-Djava.util.logging.config.file=./logging.properties"
 
-java -classpath target/classes $gclog_options $heap_options jpt.app01.Main -p7666 -q0
+java -classpath target/classes $gclog_options $heap_options $logging_options jpt.app01.Main -p7666 -q0
