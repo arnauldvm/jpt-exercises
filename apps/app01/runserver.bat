d1e94e1b53018fb6fcb7f204f61226d6f0b6daa8
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2015-10-26 01:20:15 +0100

    fix(apps/app01): add jmx options to server to enable profiling with jvisualvm (CMD)

--- apps/app01/runserver.bat
+++ apps/app01/runserver.bat
@@ -7,5 +7,8 @@
 @SET "gclog_options=-Xloggc:%logs_dir%\gc-%ts%.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
 @SET "heap_options=-Xms1g -Xmx2g"
 @SET "logging_options=-Djava.util.logging.config.file=./logging.properties"
+@SET "jmx_options=-Dcom.sun.management.jmxremote.port=6789"
+@SET "jmx_options=%jmx_options% -Dcom.sun.management.jmxremote.ssl=false"
+@SET "jmx_options=%jmx_options% -Dcom.sun.management.jmxremote.authenticate=false"
 
-java -classpath target/classes %gclog_options% %heap_options% %logging_options% jpt.app01.Main -p7666 -q0
+java -classpath target/classes %gclog_options% %heap_options% %logging_options% %jmx_options% jpt.app01.Main -p7666 -q0
