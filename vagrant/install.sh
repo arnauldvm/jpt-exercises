6677160ef575923b4886bb24e14850e3e0943743
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-03-19 03:43:58 +0100

    fix(build/vagrant): set proxy name dynamically

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -2,6 +2,18 @@
 
 # Use cygwin.
 
+PROXY_PORT=8888
+PROXY_HOST=$(hostname)
+
+# ? Required on host for VBGuestAdditions download ?
+export http_proxy="http://$PROXY_HOST:$PROXY_PORT"
+export https_proxy="$http_proxy"
+export HTTP_PROXY="$http_proxy"
+export HTTPS_PROXY="$http_proxy"
+# Required on guest for yum packages management
+export VAGRANT_HTTP_PROXY="$http_proxy"
+export VAGRANT_HTTPS_PROXY="$http_proxy"
+
 vagrant plugin install vagrant-proxyconf
 vagrant up
 
