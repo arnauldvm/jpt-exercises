fef7c975b031823724a7992680cdd38b5f84446c
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-03-19 02:44:25 +0100

    fix(build/vagrant): VBGuestAdditions now available (since proxy has been fixed)

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -5,8 +5,6 @@
 vagrant plugin install vagrant-proxyconf
 vagrant up
 # This will likely fail the first time => run it again
-vagrant plugin install vagrant-vbguest
-vagrant plugin update vagrant-vbguest
 vagrant halt
 vagrant up
 vagrant up
